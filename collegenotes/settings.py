809f65f9e45505ba3a23217bab3306264558e83b
Mohit Sharma <mohitsharma10618@gmail.com>
2026-01-08 00:51:54 +0530

    Add dj-database-url for postgres config

--- collegenotes/settings.py
+++ collegenotes/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -59,12 +60,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'collegenotes.wsgi.application'
 
-# simple sqlite for development
+# postgresql db config
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # keep empty for dev; add validators in production
