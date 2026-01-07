e3a989542db7f34a833693357abe10c99310c061
Mohit Sharma <mohitsharma10618@gmail.com>
2026-01-08 03:06:54 +0530

    Fix ALLOWED_HOSTS and CSRF trusted origins

--- collegenotes/settings.py
+++ collegenotes/settings.py
@@ -11,8 +11,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(","),
-"http://localhost:8000"
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 
 # apps
 INSTALLED_APPS = [
