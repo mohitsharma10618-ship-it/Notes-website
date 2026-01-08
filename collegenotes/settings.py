cc12bc10a142df8e445f47c67366eb3ac6c0ee5d
Mohit Sharma <mohitsharma10618@gmail.com>
2026-01-08 20:51:32 +0530

    Add superuser

--- collegenotes/settings.py
+++ collegenotes/settings.py
@@ -114,3 +114,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'auth-toggle'
 LOGIN_URL = '/accounts/login/'
+
+if os.environ.get("RENDER"):
+    try:
+        from create_superuser import *
+    except Exception as e:
+        print(e)
--- create_superuser.py
+++ create_superuser.py
@@ -9,7 +9,11 @@ password = os.environ.get("DJ_SUPERUSER_PASSWORD")
 
 if username and password:
     if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
         print("Superuser created!")
     else:
         print("Superuser already exists.")
