d4139cc74224aec98b3cd658e2b7488c461236f5
Mohit Sharma <mohitsharma10618@gmail.com>
2026-01-19 23:35:39 +0530

    remove admin creation endpoint and secure admin creation via signals

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .utils import create_admin_once
+
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('auth/', views.auth_toggle, name='auth_toggle'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('create-admin/', views.create_admin, name='create_admin'),
 ]
--- accounts/utils.py
+++ accounts/utils.py
@@ -1,20 +1,24 @@
-from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
-def create_admin_once():
-    # Safety check: env vars exist honi chahiye
+def create_admin_once(request):
+    User = get_user_model()
+
+    if User.objects.filter(is_superuser=True).exists():
+        return HttpResponse("Admin already exists")
+
     if not all([
         settings.ADMIN_USERNAME,
         settings.ADMIN_EMAIL,
         settings.ADMIN_PASSWORD
     ]):
-        return
+        return HttpResponse("Admin env vars missing", status=500)
 
-    User = get_user_model()
+    User.objects.create_superuser(
+        username=settings.ADMIN_USERNAME,
+        email=settings.ADMIN_EMAIL,
+        password=settings.ADMIN_PASSWORD
+    )
 
-    if not User.objects.filter(is_superuser=True).exists():
-        User.objects.create_superuser(
-            username=settings.ADMIN_USERNAME,
-            email=settings.ADMIN_EMAIL,
-            password=settings.ADMIN_PASSWORD
-        )
+    return HttpResponse("Admin created successfully")
